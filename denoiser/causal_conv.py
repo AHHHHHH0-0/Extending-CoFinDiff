355d53d6768135c486b589d90c2f53a5fb4a595d
Andrew Liu <landrew1018@gmail.com>
2025-12-27 21:42:57 +0800

    custome convolution to handle left padding to ensure causality and limit future leakage

---
+++ denoiser/causal_conv.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from config import config
+
+
+class CausalConv1d(nn.Conv1d):
+    """
+    Causal 1D convolution with dilation support, left padding for causality.
+    """
+    
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = config.KERNEL_SIZE,
+        dilation: int = config.DILATION,
+        **kwargs
+    ):
+        # Remove padding from kwargs
+        kwargs.pop('padding', None)
+
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            dilation=dilation,
+            padding=0,
+            **kwargs
+        )
+        # Left padding 
+        self.left_padding = (kernel_size - 1) * dilation
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Left padding 
+        x = F.pad(x, (self.left_padding, 0))
+        return super().forward(x)
+
+
