0c86251111184ddeb2f25b5e29ef0375bafe0c8a
Andrew Liu <landrew1018@gmail.com>
2025-12-27 11:49:15 +0800

    remove time embedding from diffusion folder

--- diffusion/utils.py
+++ diffusion/utils.py
@@ -1,28 +1,12 @@
 """
 Utility functions.
+- Get noise schedule (beta values) for diffusion.
 """
 
 import math
 import torch
-import torch.nn.functional as F
 
 from config.config import config
-
-
-def timestep_embedding(t: torch.Tensor, dim: int) -> torch.Tensor:
-    """
-    Create sinusoidal timestep embeddings for diffusion.
-    """
-    half = dim // 2
-    freqs = torch.exp(-math.log(10000) * torch.arange(0, half, device=t.device) / half)
-    args = t[:, None].float() * freqs[None]
-    emb = torch.cat([torch.sin(args), torch.cos(args)], dim=-1)
-    
-    # Pad 1 for odd dimensions
-    if dim % 2:
-        emb = F.pad(emb, (0, 1))
-        
-    return emb
 
 
 def get_beta_schedule(
