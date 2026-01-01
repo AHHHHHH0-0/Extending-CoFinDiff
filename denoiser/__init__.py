fa5957b6c449c86348c52800b55e0aebef7d5be1
Andrew Liu <landrew1018@gmail.com>
2026-01-02 00:54:17 +0800

    unet denoiser model inits

--- denoiser/__init__.py
+++ denoiser/__init__.py
@@ -1,17 +1,9 @@
 """
-This module contains the TCN-based denoiser network and related components
-for the conditional financial diffusion model.
-- DDPM (Denoising Diffusion Probabilistic Models)
-- TCN (Temporal Convolutional Network) denoiser
-- Causal dilated convolutions
-- Residual blocks with FiLM conditioning
-- MacroEncoder for conditioning
+CoFinDiff U-Net Denoiser Module with denoiser network and related components (U-net blocks).
 """
 
-from .denoiser_model import CoFinDiffTCN
-from .embeddings.macro_var import MacroEncoder
+from .unet_model import UNetDenoiser
 
 __all__ = [
-    "CoFinDiffTCN", 
-    "MacroEncoder"
+    "UNetDenoiser"
 ]
--- denoiser/unet_blocks/__init__.py
+++ denoiser/unet_blocks/__init__.py
@@ -5,9 +5,11 @@ U-Net blocks for the denoiser.
 from .decoder_block import DecoderBlock
 from .encoder_block import EncoderBlock
 from .residual_block import ResBlock
+from .cross_attention import SpatialCrossAttention
 
 __all__ = [
     "EncoderBlock",
     "DecoderBlock",
     "ResBlock",
+    "SpatialCrossAttention",
 ]
