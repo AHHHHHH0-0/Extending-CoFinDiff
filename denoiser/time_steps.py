70efe6c22790eded67539892c11aee74b9cf5ad1
Andrew Liu <landrew1018@gmail.com>
2026-01-02 01:08:29 +0800

    time steps embedding

---
+++ denoiser/time_steps.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn.functional as F
+import math
+
+
+def timestep_embedding(t: torch.Tensor, dim: int, max_period: int) -> torch.Tensor:
+    """
+    Create sinusoidal timestep embeddings.
+    """
+    half = dim // 2
+    freqs = torch.exp(-math.log(max_period) * torch.arange(0, half, device=t.device) / half)
+    args = t[:, None] * freqs[None]
+    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+    
+    # Pad 1 for odd dimensions
+    if dim % 2:
+        embedding = F.pad(embedding, (0, 1))
+    
+    return embedding
+    
