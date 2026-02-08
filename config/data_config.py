a844e4fab29aebd90cdcfabc740f10adf42024d8
Andrew Liu <landrew1018@gmail.com>
2026-02-08 00:51:13 -0800

    data pipeline done, just need to load actual data with actual window

--- config/data_config.py
+++ config/data_config.py
@@ -1,2 +1,4 @@
 TICKERS = ["NVDA", "GOOG", "AAPL"]
 AV_FUNCTION = "TIME_SERIES_DAILY" # change saved json file name based on function
+WINDOW_START = "2025-09-17"
+WINDOW_END = "2026-02-05"
--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,10 +1,10 @@
 """
 Preprocessing utilities for financial time series data.
-- Haar wavelet transform for converting 1D time series to 2D images.
 - Preprocess prices with log returns and standardization.
+- Haar wavelet transform for converting 1D time series to 2D images.
 """
 
 __all__ = [
-    'HaarWaveletTransform',
     'preprocess_prices',
+    'HaarWaveletTransform',
 ]
