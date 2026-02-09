cd0beba151d4fee7e78ac20153e965cefbb9bac8
Andrew Liu <landrew1018@gmail.com>
2026-02-08 22:18:16 -0800

    specify window in preprocess

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -2,9 +2,15 @@
 Preprocessing utilities for financial time series data.
 - Preprocess prices with log returns and standardization.
 - Haar wavelet transform for converting 1D time series to 2D images.
+- Condition encoder for cross-attention conditioning.
 """
+
+from .preprocess_prices import preprocess_prices
+from .condition_encoder import ConditionEncoder
+from .haar_wavelet import HaarWaveletTransform
 
 __all__ = [
     'preprocess_prices',
     'HaarWaveletTransform',
+    'ConditionEncoder',
 ]
