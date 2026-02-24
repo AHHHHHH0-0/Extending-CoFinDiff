91449aa57166b88ce9f315690a9e21d9cd76b58a
Andrew Liu <landrew1018@gmail.com>
2026-02-24 15:50:14 -0800

    100 data tickers

--- config/data_config.py
+++ config/data_config.py
@@ -2,8 +2,109 @@
 Configuration for the data.
 """
 
-TICKERS = ["NVDA", "GOOG", "AAPL", "META", "MSFT", "AMZN", "TSLA", "NFLX", "ASML", "TSM"]
 DAILY = "TIME_SERIES_DAILY"
 DAILY_ADJUSTED = "TIME_SERIES_DAILY_ADJUSTED"
-WINDOW_START = "2010-01-01"
+WINDOW_START = "2000-01-01"
 WINDOW_END = "2019-12-31"
+TICKERS = [
+    "NVDA",   # NVIDIA
+    "AAPL",   # Apple
+    "GOOG",   # Alphabet (Google)
+    "MSFT",   # Microsoft
+    "AMZN",   # Amazon
+    "META",   # Meta Platforms
+    "AVGO",   # Broadcom
+    "TSLA",   # Tesla
+    "BRK-B",  # Berkshire Hathaway
+    "WMT",    # Walmart
+    "LLY",    # Eli Lilly
+    "JPM",    # JPMorgan Chase
+    "XOM",    # Exxon Mobil
+    "JNJ",    # Johnson & Johnson
+    "V",      # Visa
+    "MU",     # Micron Technology
+    "MA",     # Mastercard
+    "COST",   # Costco
+    "ORCL",   # Oracle
+    "ABBV",   # AbbVie
+    "PG",     # Procter & Gamble
+    "HD",     # Home Depot
+    "CVX",    # Chevron
+    "BAC",    # Bank of America
+    "GE",     # General Electric
+    "CAT",    # Caterpillar
+    "AMD",    # Advanced Micro Devices
+    "KO",     # Coca-Cola
+    "NFLX",   # Netflix
+    "MRK",    # Merck
+    "CSCO",   # Cisco
+    "PLTR",   # Palantir
+    "LRCX",   # Lam Research
+    "AMAT",   # Applied Materials
+    "PM",     # Philip Morris International
+    "GS",     # Goldman Sachs
+    "MS",     # Morgan Stanley
+    "RTX",    # RTX
+    "WFC",    # Wells Fargo
+    "TMUS",   # T-Mobile US
+    "UNH",    # UnitedHealth
+    "GEV",    # GE Vernova
+    "MCD",    # McDonald's
+    "PEP",    # PepsiCo
+    "INTC",   # Intel
+    "AXP",    # American Express
+    "IBM",    # IBM
+    "VZ",     # Verizon
+    "AMGN",   # Amgen
+    "T",      # AT&T
+    "ABT",    # Abbott Laboratories
+    "NEE",    # Nextera Energy
+    "KLAC",   # KLA
+    "C",      # Citigroup
+    "TXN",    # Texas Instruments
+    "TMO",    # Thermo Fisher Scientific
+    "DIS",    # Walt Disney
+    "APH",    # Amphenol
+    "BA",     # Boeing
+    "GILD",   # Gilead Sciences
+    "CRM",    # Salesforce
+    "TJX",    # TJX Companies
+    "ISRG",   # Intuitive Surgical
+    "DE",     # Deere & Company
+    "ADI",    # Analog Devices
+    "SCCO",   # Southern Copper
+    "SCHW",   # Charles Schwab
+    "BLK",    # BlackRock
+    "HON",    # Honeywell
+    "ANET",   # Arista Networks
+    "UNP",    # Union Pacific
+    "LOW",    # Lowe's
+    "QCOM",   # Qualcomm
+    "PFE",    # Pfizer
+    "LMT",    # Lockheed Martin
+    "UBER",   # Uber
+    "DHR",    # Danaher
+    "WELL",   # Welltower
+    "SYK",    # Stryker
+    "BX",     # Blackstone
+    "NEM",    # Newmont
+    "COP",    # ConocoPhillips
+    "APP",    # AppLovin
+    "BKNG",   # Booking Holdings
+    "PLD",    # Prologis
+    "GLW",    # Corning
+    "PH",     # Parker-Hannifin
+    "SPGI",   # S&P Global
+    "BMY",    # Bristol-Myers Squibb
+    "COF",    # Capital One
+    "VRTX",   # Vertex Pharmaceuticals
+    "IBKR",   # Interactive Brokers
+    "PGR",    # Progressive
+    "MCK",    # McKesson
+    "HCA",    # HCA Healthcare
+    "MO",     # Altria Group
+    "CMCSA",  # Comcast
+    "CME",    # CME Group
+    "PANW",   # Palo Alto Networks
+    "CEG",    # Constellation Energy
+]
