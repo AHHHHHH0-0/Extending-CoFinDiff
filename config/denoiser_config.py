d230c0b4d6c5f1f97b0342e7e5a0417a493f2a14
Andrew Liu <landrew1018@gmail.com>
2026-02-25 22:38:29 -0800

    change base channels from 20 to 32 bc of some errors

--- config/denoiser_config.py
+++ config/denoiser_config.py
@@ -20,9 +20,9 @@ RES_BLOCK_DROPOUT = 0.0
 # Cross-attention parameters
 CROSS_ATTN_NUM_HEADS = 4
 CROSS_ATTN_SCALE = 0.1 
-COND_CONTEXT_DIM = 20 # must match COND_OUTPUT_DIM in preprocess_config
+COND_CONTEXT_DIM = 32 # must match COND_OUTPUT_DIM in preprocess_config
 
 # U-Net model parameters
 TIME_EMBED_DIM = 256
-BASE_CHANNELS = 20
+BASE_CHANNELS = 32
 CHANNEL_MULT = [1, 2, 4, 8]
