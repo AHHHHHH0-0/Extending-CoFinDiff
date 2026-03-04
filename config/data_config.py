7bd86f9c74e3e7ca1d51dbc3f93c42e41dadfd0f
Andrew Liu <landrew1018@gmail.com>
2026-03-04 14:51:27 -0800

    100 next tickers

--- config/data_config.py
+++ config/data_config.py
@@ -108,3 +108,107 @@ TICKERS = [
     "PANW",   # Palo Alto Networks
     "CEG",    # Constellation Energy
 ]
+TICKERS2 = [
+    "TSM",    # Taiwan Semiconductor
+    "ASML",   # ASML Holding
+    "TM",     # Toyota Motor
+    "BABA",   # Alibaba
+    "NVS",    # Novartis
+    "AZN",    # AstraZeneca
+    "HSBC",   # HSBC Holdings
+    "SHEL",   # Shell
+    "LIN",    # Linde
+    "RY",     # Royal Bank of Canada
+    "SAP",    # SAP
+    "BHP",    # BHP Group
+    "MUFG",   # Mitsubishi UFJ Financial
+    "SAN",    # Banco Santander
+    "TTE",    # TotalEnergies
+    "RIO",    # Rio Tinto
+    "NVO",    # Novo Nordisk
+    "TD",     # Toronto-Dominion Bank
+    "UL",     # Unilever
+    "SHOP",   # Shopify
+    "BUD",    # Anheuser-Busch InBev
+    "HDB",    # HDFC Bank
+    "ETN",    # Eaton
+    "PDD",    # PDD Holdings
+    "SMFG",   # Sumitomo Mitsui Financial
+    "SONY",   # Sony Group
+    "BTI",    # British American Tobacco
+    "CB",     # Chubb
+    "ARM",    # Arm Holdings
+    "ACN",    # Accenture
+    "AEM",    # Agnico Eagle Mines
+    "UBS",    # UBS Group
+    "MDT",    # Medtronic
+    "BBVA",   # Banco Bilbao Vizcaya Argentaria
+    "ENB",    # Enbridge
+    "GSK",    # GSK
+    "INTU",   # Intuit
+    "SNY",    # Sanofi
+    "NOW",    # ServiceNow
+    "BSX",    # Boston Scientific
+    "SBUX",   # Starbucks
+    "NOC",    # Northrop Grumman
+    "SO",     # Southern Company
+    "IBN",    # ICICI Bank
+    "PBR",    # Petrobras
+    "ADBE",   # Adobe
+    "HWM",    # Howmet Aerospace
+    "MFG",    # Mizuho Financial
+    "SPOT",   # Spotify
+    "CVS",    # CVS Health
+    "TT",     # Trane Technologies
+    "BMO",    # Bank of Montreal
+    "DUK",    # Duke Energy
+    "DELL",   # Dell Technologies
+    "BP",     # BP
+    "VRT",    # Vertiv Holdings
+    "GD",     # General Dynamics
+    "BN",     # Brookfield Corporation
+    "FCX",    # Freeport-McMoRan
+    "WM",     # Waste Management
+    "UPS",    # UPS
+    "CRWD",   # CrowdStrike
+    "EQIX",   # Equinix
+    "ITUB",   # Itaú Unibanco
+    "ICE",    # Intercontinental Exchange
+    "WMB",    # Williams Companies
+    "CM",     # Canadian Imperial Bank of Commerce
+    "NGG",    # National Grid
+    "BNS",    # Bank of Nova Scotia
+    "CNQ",    # Canadian Natural Resources
+    "WDC",    # Western Digital
+    "FDX",    # FedEx
+    "NKE",    # Nike
+    "MELI",   # MercadoLibre
+    "JCI",    # Johnson Controls
+    "AMT",    # American Tower
+    "SHW",    # Sherwin-Williams
+    "MAR",    # Marriott International
+    "ADP",    # Automatic Data Processing
+    "PNC",    # PNC Financial
+    "PWR",    # Quanta Services
+    "ECL",    # Ecolab
+    "EMR",    # Emerson Electric
+    "MMM",    # 3M
+    "STX",    # Seagate Technology
+    "USB",    # US Bancorp
+    "ITW",    # Illinois Tool Works
+    "CDNS",   # Cadence Design Systems
+    "MCO",    # Moody's
+    "BK",     # Bank of New York Mellon
+    "RCL",    # Royal Caribbean
+    "SNPS",   # Synopsys
+    "REGN",   # Regeneron Pharmaceuticals
+    "KKR",    # KKR & Co
+    "CTAS",   # Cintas
+    "MSI",    # Motorola Solutions
+    "CSX",    # CSX
+    "CMI",    # Cummins
+    "ABNB",   # Airbnb
+    "ORLY",   # O'Reilly Automotive
+    "MNST",   # Monster Beverage
+    "CL",     # Colgate-Palmolive
+]
